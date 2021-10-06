558a4c840abcd1d7386627ca33e7478589d53c1f
Omlet144 <alexblack144@gmail.com>
2021-10-06 20:35:29 +0300

    class Room

---
+++ Room/User.cs
@@ -0,0 +1,12 @@
+namespace HW3
+{
+    public class User
+    {
+        public RemoteControl RemoteControler;
+
+    public string RemoteControlOperation(RemoteControl RemoteControler)
+	    {
+		    return $"You take controler";
+	    }
+    }
+}
