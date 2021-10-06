0df2b2dc769befb02dcc1615a126d8b5735a158e
Omlet144 <alexblack144@gmail.com>
2021-10-06 20:44:51 +0300

    release Main

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace Condition
+namespace HW3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Room room = new Room(26);
+            TemperatureSensor sensor = new TemperatureSensor(room);
+            RemoteControl controller = new RemoteControl("On", 15);
+            User user = new User();
+            AirConditioning condition = new AirConditioning();
+
+
+            Console.WriteLine($"{user.RemoteControlOperation(controller)}");
+            Console.WriteLine($"{controller.PowerOnOrOff()}");
+            Console.WriteLine($"{condition.PowerOnOrOff(controller)}");
+            Console.WriteLine($"{sensor.GetInfo(controller)}");
+            Console.WriteLine($"{controller.SetTheDesiredTemperature()}");
+            Console.WriteLine($"{condition.ShowInfo(controller)}");
+            
+
+            Console.Read();
         }
     }
 }
